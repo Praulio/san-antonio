bd953af960a656492d3a1bf0a97c8ee527d7af2d
Praulio <prickypraulio@gmail.com>
2026-05-05 13:12:07 -0600

    config: switch to gviz CSV endpoint with real Sheet ID

--- src/config.js
+++ src/config.js
@@ -5,15 +5,15 @@ export const GROUP_NAME = cfg.groupName ?? 'San Antonio';
 export const APPS_SCRIPT_URL = cfg.appsScriptUrl;
 
 export function csvUrl(sheetGid) {
-  if (!cfg.sheetCsvBase || cfg.sheetCsvBase.startsWith('PUT_')) {
-    throw new Error('Configura sheetCsvBase en index.html');
+  if (!cfg.sheetId || cfg.sheetId.startsWith('PUT_')) {
+    throw new Error('Configura sheetId en index.html');
   }
-  return `${cfg.sheetCsvBase}&gid=${sheetGid}`;
+  return `https://docs.google.com/spreadsheets/d/${cfg.sheetId}/gviz/tq?tqx=out:csv&gid=${sheetGid}`;
 }
 
 export const SHEET_GIDS = {
-  participantes: '0',
-  turnos: '1',
-  testimonios: '2',
-  config: '3'
+  participantes: '1001',
+  turnos: '1002',
+  testimonios: '1003',
+  config: '1004'
 };
